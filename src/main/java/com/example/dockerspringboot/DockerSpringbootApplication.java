cec5d4f127bdc0137d9dccfdcd6b120f3ef207a6
Puri12 <tkdqja9812@gmail.com>
2023-01-14 19:22:21 +0900

    init : spring boot init

---
+++ src/main/java/com/example/dockerspringboot/DockerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSpringbootApplication.class, args);
+    }
+
+}
